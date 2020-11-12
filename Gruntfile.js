437f204fd0b0c3590781e1b02bea8da7578f0385
Muzammil Shakir <muzammil.shakir786@gmail.com>
2020-11-12 21:56:12 +0500

    port

--- Gruntfile.js
+++ Gruntfile.js
@@ -98,7 +98,7 @@ module.exports = function(grunt) {
     connect: {
       server: {
         options: {
-          port: 8000,
+          port: process.env.PORT || 8000,
           keepalive: false
         }
       }
